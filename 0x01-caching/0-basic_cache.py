8159a7adca29998121cbcde7338de8953d38b16c
ogordiunor uche jude <excelxz78@gmail.com>
2024-07-25 02:43:40 +0000

    cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -37,4 +37,4 @@ class BasicCache(BaseCaching):
         """
         if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
-        return None return None
+        return None
